99e2020881f1ce8b71e64def9c8c33c613681a9f
Cameron Westland <camwest@gmail.com>
2013-06-04 14:33:20 -0400

    Extracted js/css building into cog.js

--- index.js
+++ index.js
@@ -1,49 +1,18 @@
 var express = require('express')
-  , fs = require('fs')
   , app = express()
-  , buildJs = require('./lib/buildJs')
-  , buildCss = require('./lib/buildCss');
+  , cog = require('./src/cog');
 
 app.use(express.logger());
-app.use('/client/public', express.static(__dirname + '/client/public'));
 app.use('/client/admin', express.static(__dirname + '/client/admin/templates'));
 app.use(function(err, req, res, next) {
   console.log(error.stack);
   res.send(500, 'Oops!');
 });
 
-app.get('/cog.js', function(req, res) {
-  buildJs(function(err, js) {
-    if (err) {
-      throw err;
-    }
-
-    res.setHeader('Content-Type', 'text/javascript; charset=UTF-8');
-    res.send(js);
-  });
-});
-
-app.get('/cog.css', function(req, res) {
-  buildCss(function(err, css) {
-    if (err) {
-      throw err;
-    }
-
-    res.setHeader('Content-TYpe', 'text/css; charset=UTF-8');
-    res.send(css);
-  });
-});
-
-app.get('*', function(req, res) {
-  fs.readFile(__dirname + '/client/theme/theme.html', function(err, html) {
-    if (err) {
-      throw err;
-    }
-
-    res.type('.html');
-    res.send(html);
-  });
-});
+// COG MAIN
+app.get('/cog.js', cog.js);
+app.get('/cog.css', cog.css);
+app.get('*', cog.index);
 
 var port = process.env.PORT || 5000;
 
---
+++ src/cog.js
@@ -0,0 +1,43 @@
+var buildJs = require('../lib/buildJs')
+  , buildCss = require('../lib/buildCss')
+  , fs = require('fs');
+
+function js(req, res) {
+  buildJs(function(err, js) {
+    if (err) {
+      throw err;
+    }
+
+    res.setHeader('Content-Type', 'text/javascript; charset=UTF-8');
+    res.send(js);
+  });
+}
+
+function css(req, res) {
+  buildCss(function(err, css) {
+    if (err) {
+      throw err;
+    }
+
+    res.setHeader('Content-Type', 'text/css; charset=UTF-8');
+    res.send(css);
+  });
+
+}
+
+function index(req, res) {
+  fs.readFile(__dirname + '/../client/theme/theme.html', function(err, html) {
+    if (err) {
+      throw err;
+    }
+
+    res.type('.html');
+    res.send(html);
+  });
+}
+
+module.exports = {
+  js: js,
+  css: css,
+  index: index
+};
