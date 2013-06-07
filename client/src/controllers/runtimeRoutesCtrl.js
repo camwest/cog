2cf47deb4d478c0cc9f1f6d60b87036fe74f3f85
Cameron Westland <camwest@gmail.com>
2013-06-06 20:58:59 -0400

    Refactored window to $window

--- client/src/controllers/runtimeRoutesCtrl.js
+++ client/src/controllers/runtimeRoutesCtrl.js
@@ -1,5 +1,5 @@
-angular.module('cog').controller('RuntimeRoutesCtrl', ['$scope', '$route', '$rootScope', function($scope, $route, $rootScope) {
-  var pathName = window.location.pathname;
+angular.module('cog').controller('RuntimeRoutesCtrl', ['$rootScope', '$window', function($rootScope, $window) {
+  var pathName = $window.location.pathname;
 
   if (pathName === '/') {
     pathName = '/index';
