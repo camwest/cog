d0cabd8c668521995a970619116ac040f4aa3c4c
Cameron Westland <camwest@gmail.com>
2013-08-01 15:27:23 -0400

    Fixed up sample theme

--- client/src/directives/cogField.js
+++ client/src/directives/cogField.js
@@ -12,20 +12,5 @@
       }
     };
   }]);
-
-  angular.module('cog').directive('field', ['FieldLinker', function(FieldLinker) {
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
-        if (element.prop('tagName') !== 'COG') {
-          return;
-        }
-
-        var fieldLabel = element.html();
-        var fieldType = attrs.field;
-        var linker = new FieldLinker(scope, element, fieldLabel, fieldType);
-      }
-    };
-  }]);
 }(angular));
 
