5a98c4a7ed19023d995da15bb68537668900e3bf
Cameron Westland <camwest@gmail.com>
2013-08-02 14:56:39 -0400

    cog include now automatically creates a section in the CMS

--- client/src/directives/cogInclude.js
+++ client/src/directives/cogInclude.js
@@ -1,13 +1,14 @@
 angular.module('cog').directive('include', function() {
   return {
     restrict: 'A',
-    scope: {},
-    template: '<ng-include src="includeUrl"></ng-include>',
+    template: '<div cog-section="{{ includeName }}"><ng-include src="includeUrl"></ng-include></div>',
+    scope: true,
     link: function(scope, element, attrs) {
       if (element.prop('tagName') !== 'COG') {
         return;
       }
 
+      scope.includeName = attrs.include;
       scope.includeUrl = '/includes/' + attrs.include + '.html';
     }
   };
--- client/src/directives/cogSection.js
+++ client/src/directives/cogSection.js
@@ -1,10 +1,16 @@
 angular.module('cog').directive('cogSection', ['SiteLoader', function(SiteLoader) {
   return {
     restrict: 'A',
-    scope: {},
+    scope: true,
     link: function(scope, element, attrs) {
-      var rawElement = element[0];
-      scope.cogSection = SiteLoader.fetchSection(rawElement, attrs.cogSection);
+      attrs.$observe('cogSection', function(sectionName) {
+        if (!sectionName) {
+          return;
+        }
+
+        var rawElement = element[0];
+        scope.cogSection = SiteLoader.fetchSection(rawElement, sectionName);
+      });
     }
   };
 }]);
